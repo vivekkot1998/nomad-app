3bb6a21c784ace3eca33136ae8374e11f885cd7a
unknown <vivekkot1998@gmail.com>
2022-11-29 23:54:02 +0530

    Added the places service containing dummy data.

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './places.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Manhattan Mansion',
+      'In the heart of New York City.',
+      'https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzL2RjN2NiYjRjZDAyNGNhNjE1NF9JTUdfOTQ5OC5KUEciXSxbInAiLCJ0aHVtYiIsIngzOTA-Il0sWyJwIiwiY29udmVydCIsIi1xdWFsaXR5IDgxIC1hdXRvLW9yaWVudCJdXQ',
+      149.99
+    ),
+    new Place(
+      'p2',
+      "L'Amour Toujours",
+      'A romantic place in Paris!',
+      'https://media-cdn.tripadvisor.com/media/photo-s/16/0b/96/09/hotel-amour.jpg',
+      189.99
+      ),
+    new Place(
+      'p3',
+      "The Foggy Place",
+      'Not your average city trip!',
+      'https://thumbs.dreamstime.com/b/amazing-foggy-autumn-day-landscape-high-mountains-forest-pine-trees-early-morning-mist-touristic-place-amazing-195479608.jpg',
+      99.99
+      )
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() { }
+}
